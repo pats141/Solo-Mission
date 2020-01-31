3bb2b789f01c8ba520a36db53f3703043369b52d
Evan <evan@alpharho.com>
2020-01-31 10:47:29 -0500

    Initial Commit

---
+++ Solo MissionTests/Solo_MissionTests.swift
@@ -0,0 +1,34 @@
+//
+//  Solo_MissionTests.swift
+//  Solo MissionTests
+//
+//  Created by Evan Tall on 1/31/20.
+//  Copyright © 2020 Evan Tall. All rights reserved.
+//
+
+import XCTest
+@testable import Solo_Mission
+
+class Solo_MissionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
